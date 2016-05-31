2228fdceffe1f05b9dc7ece95cf5794cc2d74a40
criespy <riespratama@gmail.com>
2016-05-31 11:55:42 +0700

    Bikin model Item, dan migrate database

--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Item(models.Model):
+    item_id = models.CharField(max_length=8, primary_key=True)
+    description = models.CharField(max_length=128)
+    group = models.CharField(max_length=32)
+    status = models.CharField(max_length=16)
+    location = models.CharField(max_length=16)
+    created_date = models.DateTimeField(default=timezone.now)
+    last_modified = models.DateTimeField(blank=True, null=True)
+    last_mod_by = models.CharField(max_length=16)
+
+    def modified(self):
+        self.last_modified = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.description
+
+    #user = models.ForeignKey('auth.User')
